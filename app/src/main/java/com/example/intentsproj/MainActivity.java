aae320bc6c7a0c8d45f6b1c93cf132c02b8f1d8a
Dakshinda <IT19032320@my.sliit.lk>
2020-08-19 00:23:51 +0530

    cal

--- app/src/main/java/com/example/intentsproj/MainActivity.java
+++ app/src/main/java/com/example/intentsproj/MainActivity.java
@@ -4,8 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Telephony;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
